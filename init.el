edf3d3a22261d1a1ca694eaa30a2bf7035d79357
Norbert Melzer <timmelzer@gmail.com>
2016-07-27 01:21:13 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,32 @@
+;; NobbZ init.el
+;; Real stuff happens in =settings.org=
+
+(require 'package)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+
+;; Set the load path
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Moved all the custom.el stuff into its own file called
+;; =~/.emacs.d/customize.el=
+(setq custom-file "~/.emacs.d/customize.el")
+(load custom-file)
+
+(setq hostname
+      (replace-regexp-in-string "\\`[ \t\n]*" ""
+				(replace-regexp-in-string "[ \t\n]*\\'" ""
+							  (shell-command-to-string "hostname"))))
+
+(defface org-block-begin-line
+  '((t (:foreground "#99968b" :background "#303030")))
+  "Face used for line delimiting the begin of source blocks.")
+
+(defface org-block-end-line
+  '((t (:foreground "#99968b" :background "#303030")))
+  "Face used for line delimiting the end of source blocks.")
+
+(org-babel-load-file "~/.emacs.d/settings.org")
