7108e5786e04121575b600b5d7ee14e2d6ed8482
Norbert Melzer <timmelzer@gmail.com>
2016-09-05 07:38:06 +0200

    installed packages

--- customize.el
+++ customize.el
@@ -1,2 +1,16 @@
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode+ markdown-mode framemove haskell-mode puppet-mode project-explorer company which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
